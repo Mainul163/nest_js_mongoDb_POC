f62f7c2b44e1c638f3289bc85e95af7125a60ca9
mainul163 <mainulisraf163@gmail.com>
2024-03-21 00:27:54 +0600

    work

---
+++ .env
@@ -0,0 +1,1 @@
+MONGO_URI=mongodb+srv://mainulisraf163:Lu3ESWqzdNWQNUBB@cluster0.hkkkwwm.mongodb.net/DemoDatabase?retryWrites=true&w=majority
---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+    PostsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
